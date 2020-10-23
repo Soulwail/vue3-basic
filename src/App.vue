6d7f888606dd3fc9b9b447c8a77e90c87f6e690e
冫水 <Soulwail@bingshuideMacBook-Pro.local>
2020-10-23 14:57:17 +0800

    ref & reactive

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <h1>{{ count }}</h1>
+        <h1>{{ double }}</h1>
+        <button @click="increase">👍+1</button>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+import { ref, computed, reactive, toRefs } from 'vue';
+interface DataProps {
+    count: number;
+    double: number;
+    increase: () => void;
+}
+export default {
+    name: 'App',
+    setup() {
+        // const count = ref(0);
+        // const double = computed(() => count.value * 2);
+        // const increase = () => {
+        //     count.value++;
+        // };
+        const data: DataProps = reactive({
+            count: 0,
+            increase: () => data.count++,
+            double: computed(() => data.count * 2),
+        });
+        const refData = toRefs(data)
+        return {
+            // count,
+            // increase,
+            // double,
+            ...refData
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
