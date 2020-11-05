a9d7827def7d72d23b2766f00152b4be2b85399d
Soulwail <879457843@qq.com>
2020-11-05 20:28:02 +0800

    添加header组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <global-header :user="currentUser"></global-header>
         <column-list :list="list"></column-list>
     </div>
 </template>
@@ -7,6 +8,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ColumnList, { ColumnProps } from './components/ColumnList.vue';
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
+const currentUser: UserProps = {
+    isLogin: true,
+    name: 'soulwail'
+}
 const testData: ColumnProps[] = [
     { id: 1, title: 'test1的专栏', description: '这是test1的专栏' },
     { id: 2, title: 'test2的专栏', description: '这是test2的专栏' },
@@ -15,10 +21,12 @@ export default defineComponent({
     name: 'App',
     components: {
         ColumnList,
+        GlobalHeader
     },
     setup() {
         return {
             list: testData,
+            currentUser
         };
     },
 });
