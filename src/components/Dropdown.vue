84f570c0e00060ff86e86bca377e9b8599811297
Soulwail <879457843@qq.com>
2020-11-09 20:44:45 +0800

    钩子函数抽离

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -14,7 +14,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onUnmounted } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
+import useClickOutside from '../hooks/useClickOutside';
 export default defineComponent({
     name: 'Dropdown',
     props: {
@@ -29,23 +30,17 @@ export default defineComponent({
         const toggleOpen = () => {
             isOpen.value = !isOpen.value;
         };
-        const handler = (e: MouseEvent) => {
-            if (dropdownRef.value) {
-                if (!dropdownRef。value.contains(e.target as HTMLElement) && isOpen.value) {
-                    isOpen.value = false
-                }
+        const isClickOutside = useClickOutside(dropdownRef);
+
+        watch(isClickOutside, () => {
+            if (isOpen.value && isClickOutside.value) {
+                isOpen.value = false;
             }
-        };
-        onMounted(() => {
-            document.addEventListener('click', handler);
-        });
-        onUnmounted(() => {
-            document.removeEventListener('click', handler);
         });
         return {
             isOpen,
             toggleOpen,
-            dropdownRef
+            dropdownRef,
         };
     },
 });
